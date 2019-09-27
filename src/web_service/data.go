211657309a429eb80a22dcebfb655d34dbda5809
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-09-27 18:47:41 +0900

    ・database/sql関連のコメントを追加する。

--- src/web_service/data.go
+++ src/web_service/data.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	// PostgreSQLのドライバをインポートする。
 	_ "github.com/lib/pq"
 )
 
@@ -10,7 +11,9 @@ var Db *sql.DB
 
 func init() {
 	var err error
-	// 
+	// DBをオープンする。
+	// 第1引数は、データベースドライバの名前を指定する。
+	// 第2引数は、データソース名を指定する。
 	Db, err = sql.Open("postgres", "host=web_api_postgres user=gwp password=gwp dbname=gwp sslmode=disable")
 	if err != nil {
 		panic(err)
@@ -19,7 +22,10 @@ func init() {
 
 func retrieve(id int) (post Post, err error) {
 	post = Post {}
-	// 
+	// DB.QueryRowは、SQLクエリを実行する。
+	// 第1引数は、SQLクエリの文字列を指定する。
+	// 第2引数以降は、SQLクエリの変数($1,$2,..)に割り当てるデータを指定する。
+	// Row.Scanは、結果を引数のポインタに割り当てる。
 	err = Db.QueryRow("select id, content, author from posts where id =$1", id).Scan(&post.ID, &post.Content, &post.Author)
 	return
 }
@@ -28,7 +34,7 @@ func retrieve(id int) (post Post, err error) {
 // テーブルpostsに投稿を登録する。
 func (post *Post) create() (err error) {
 	statement := "insert into posts (content, author) values ($1, $2) returning id"
-	// 
+	// DB.Prepareは、引数のSQLクエリを元にStmt（プリペアードステートメント）を作成する。
 	stmt, err := Db.Prepare(statement)
 	if err != nil {
 		return
@@ -39,7 +45,9 @@ func (post *Post) create() (err error) {
 }
 
 func (post *Post) update() (err error) {
-	// 
+	// DB.Execは、指定のSQLクエリを実行する。（行結果は返却しない）
+	// 第1引数は、SQLクエリを指定する。
+	// 第2引数以降は、SQLクエリ内の変数に割り当てる値を指定する。
 	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.ID, post.Content, post.Author)
 	return
 }
